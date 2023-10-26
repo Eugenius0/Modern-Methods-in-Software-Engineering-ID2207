4e329e80c0f20a3974ebda9f0cde779fe7a41933
Eugen Lucchiari Hartz <eugenius@Eugens-MacBook-Pro.local>
2023-10-26 18:39:01 +0200

    improve login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,13 +10,13 @@ export default function Login() {
         <form>
         <h2>Login</h2>
         <div className={inputContainer}>
-        <input type="email" name="email" className={input} required />
-            <label>Email</label>
+        <input type="email" name="email" className={input} required placeholder="Email" onFocus={(e) => e.target.placeholder = ''} onBlur={(e) => e.target.placeholder = 'Email'} />
+    
             <span>Email</span>
         </div>
         <div className={inputContainer}>
-        <input type="password" name="password" className={input} required />
-            <label>Password</label>
+        <input type="password" name="password" className={input} required placeholder="Password" onFocus={(e) => e.target.placeholder = ''} onBlur={(e) => e.target.placeholder = 'Password'} />
+
             <span>Password</span>
         </div>
         <input type="submit" value="Submit" className={button} />
