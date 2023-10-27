3fae90a23164f861b2da90eff680a5ecfcd2cac9
Eugen Lucchiari Hartz <eugenius@Eugens-MacBook-Pro.local>
2023-10-27 10:37:21 +0200

    login test and improvements

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,14 +3,37 @@ import styles from './styles.module.css'
 import { authenticateUser } from '../utils/auth'
 
 export default function Login() {
-    const {logo, container, form, inputContainer, input, button} = styles
+    const {logo, container, form, inputContainer, input, button, warning} = styles
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [showWarning, setShowWarning] = useState(false)
 
     const handleLogin = () => {
-    authenticateUser(email, password) ? alert('true') : alert('false')
+    authenticateUser(email, password) ? alert('true') : setShowWarning(true)
     }
+
+          // Test function to simulate the logic
+  const runLogicTest = () => {
+    // Simulate a successful authentication
+    setEmail('user1')
+    setPassword('password1')
+    handleLogin()
+
+    // Simulate an unsuccessful authentication
+    setEmail('invalid_email')
+    setPassword('invalid_password')
+    handleLogin()
+
+    // Verify that the warning is displayed
+    if (showWarning) {
+      console.log('Test Passed: Invalid email or password. Please try again.')
+    } else {
+      console.log('Test Failed: Warning not displayed.')
+    }
+  }
+// Uncomment the next line to run the test
+// runLogicTest()
 
   return (
     <div className={container}>
@@ -54,6 +77,7 @@ export default function Login() {
           className={button} 
           onClick={handleLogin}
           />
+          {showWarning && <div className={warning}>Invalid email or password. Please try again.</div>}
         </form>
         </div>
     </div>
