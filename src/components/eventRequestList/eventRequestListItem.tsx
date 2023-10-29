0b0d4686b0a46986ee3c1415366667e43c8d298a
Eugen Lucchiari Hartz <eugenius@Eugens-MacBook-Pro.local>
2023-10-29 11:36:08 +0100

    improvements to select item mechanism

--- src/components/eventRequestList/eventRequestListItem.tsx
+++ src/components/eventRequestList/eventRequestListItem.tsx
@@ -9,24 +9,24 @@ export type EventRequestListItemProps = {
 
 export const EventRequestListItem = (props: EventRequestListItemProps) => {
 
-    const {item, details} = styles
+    const {item, details, itemSelected} = styles
     const { eventRequest, selectedItem, setSelectedItem } = props 
 
     const [showDetails, setShowDetails] = useState(false)
 
     const toggleShowDetails = () => {
       setShowDetails(!showDetails)
-      showDetails ? setSelectedItem('') : setSelectedItem(eventRequest.eventType)
+      !showDetails ? setSelectedItem(eventRequest.eventType) : setSelectedItem('')
     };
 
   return (
     <div key={eventRequest.id}>
-        <div className={item} onClick={toggleShowDetails}>
+        <div className={eventRequest.eventType === selectedItem ? itemSelected : item} onClick={toggleShowDetails}>
             {eventRequest.eventType}
             <br /> <br />
             from {eventRequest.startDate} to {eventRequest.endDate}
         </div>
-        {showDetails && (
+        {eventRequest.eventType === selectedItem && showDetails && (
           <div className={details}>
           Client Name: {eventRequest.clientName} <br />
           Expected Attendees: {eventRequest.expectedAttendees} <br />
